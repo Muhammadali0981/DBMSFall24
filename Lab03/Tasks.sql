98c8beb7a5b4feee150020978cfe8ce91d36cafc
Muhammadali0981 <muhammadali030605@gmail.com>
2025-10-09 01:53:19 +0500

    lab 3

--- Lab03/Tasks.sql
+++ Lab03/Tasks.sql
@@ -1,5 +1,3 @@
-create user ali identified by ali;
-
 create table employees(
     emp_id varchar(255) not null primary key,
     emp_name varchar(255),
@@ -7,4 +5,12 @@ create table employees(
     constraint salary_range check (salary > 20000),
     dept_id varchar(255)
 );
+Alter table employees rename column emp_name TO full_name;
+Alter table employees drop constraint salary_range;
+Insert into employees values ('k19', 'Ali', 50000, 'd12'); 
 
+create table department{
+    dep_id varchar(255) not null primary key,
+    
+
+};
